1e6d80f984a13538b93a2f686ac6091d15a0fb13
mulholio <james@jmulholland.com>
2021-04-06 08:28:21 +0100

    send coord_move message

--- chat-frontend/src/App.js
+++ chat-frontend/src/App.js
@@ -6,17 +6,17 @@ const WEB_SOCKET_URL = 'ws://localhost:4000/socket';
 
 
 function App() {
+  // useSocket
   const [socket, setSocket] = useState();
-
   useEffect(() => {
     const socket = new Socket(WEB_SOCKET_URL, {params: {}})
     socket.connect();
     setSocket(socket);
   }, []);
 
-  const [channel, setChannel] = useState();
-  console.log('channel', channel);
 
+  // useChannel
+  const [channel, setChannel] = useState();
   useEffect(() => {
     if (!socket) return
     const newChannel = socket.channel("room:jazz", {});
@@ -24,6 +24,7 @@ function App() {
     newChannel.join()
       .receive("ok", resp => {
         console.log("Joined successfully", resp)
+        setChannel(newChannel);
       })
       .receive("error", resp => {
         console.log("Unable to join", resp)
@@ -31,10 +32,15 @@ function App() {
     return () => newChannel.leave();
   }, [setChannel, socket]);
 
+  const move = () => {
+    console.log('channel', channel)
+    channel && channel.push("coord_move", { coords: { x: 20, y: 20 }})
+  }
 
   return (
     <div className="App">
       <h1>Talk 'n' chat!</h1>
+      <button onClick={move}>Move</button>
     </div>
   );
 }
